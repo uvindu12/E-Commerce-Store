ff000543917f77f571abae9596741a5a199bc9be
uvindu <uvindukithmina47@gmail.com>
2025-03-13 11:49:21 +0530

    created footer and Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { CategorySection } from "@/components/category-section"
+import { FeaturedProducts } from "@/components/featured-products"
+import { HeroSection } from "@/components/hero-section"
 import { Button } from "@/components/ui/button"
 import { ShoppingBag } from "lucide-react"
 import Link from "next/link"
@@ -6,7 +9,7 @@ import Link from "next/link"
 export default function Home () {
   return (
     <div className = "flex flex-col min-h-screen">
-      <header className = "border-b rounded-b-xl bg-blue-50 shadow-md">
+      <header className = "border-b rounded-b-xl shadow-md">
         <div className= "container flex h-16 items-center justify-between px-4 md:px-6">
           <Link href="/" className ="flex item-center gap-2 text-lg font-semibold">
           <ShoppingBag className= "h-6 w-6"/>
@@ -32,6 +35,25 @@ export default function Home () {
           </div>
         </div>
       </header>
+
+      <main className ="felx-1">
+        <HeroSection/>
+      </main>
+
+      <footer className ="border-t py-6 md:py-8 rounded-2xl shadow-md ">
+        <div className= "container flex flex-col md:flex-row items-center justify-between gap-4 md:px-6">
+          <div className ="flex items-center gap-2">
+            <ShoppingBag className ="h-6 w-6"/>
+            <span className ="text-lg font-semibold"><span className= "text-blue-600 font-bold ">Style</span> <span className ="font-bold text-blue-900">Stroe</span></span>
+          </div>
+          <p className ="text-sm text-muted-foreground">© 2025 StyleStore. All rights reserved.</p>
+          <div className ="flex gap-4">
+            <Link href="#" className ="text-sm font-medium hover:underline underline-offset-4">Terms</Link>
+            <Link href="#" className ="text-sm font-medium hover:underline underline-offset-4">Privacy</Link>
+            <Link href="#" className ="text-sm font-medium hover:underline underline-offset-4">Contact</Link>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
